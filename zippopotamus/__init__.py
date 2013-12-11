dbeee3dfaa4b654db1a5775d5e4f926583fdd4d7
Pratyush <akpratyush@gmail.com>
2013-12-11 16:01:56 +0100

    First Commit

---
+++ zippopotamus/__init__.py
@@ -0,0 +1,1 @@
+from zippopotamus import Zippopotamus
---
+++ zippopotamus/zippopotamus.py
@@ -0,0 +1,26 @@
+import urllib2, json
+
+class Zippopotamus:
+	def handleAPIError(self):
+		return False
+
+	def unicodeToUTF8(self, input):
+	    if isinstance(input, dict):
+	        return {self.unicodeToUTF8(key): self.unicodeToUTF8(value) for key, value in input.iteritems()}
+	    elif isinstance(input, list):
+	        return [self.unicodeToUTF8(element) for element in input]
+	    elif isinstance(input, unicode):
+	        return input.encode('utf-8')
+	    else:
+	        return input
+
+	def places(self, zipcode):
+		apiUrl = 'http://api.zippopotam.us/us/'
+		requestUrl = apiUrl + zipcode
+		try:
+			response = urllib2.urlopen(requestUrl)
+			result = json.load(response)
+			return self.unicodeToUTF8(result)
+		except urllib2.URLError, e:
+			print 'Request Failed for ' + zipcode
+			return False
