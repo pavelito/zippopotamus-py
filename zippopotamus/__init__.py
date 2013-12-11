d4bcd5febed0e5c878b1e695380d96958c203870
Pratyush <akpratyush@gmail.com>
2013-12-11 18:01:55 +0100

    Adding author and version information

--- zippopotamus/__init__.py
+++ zippopotamus/__init__.py
@@ -1,1 +1,13 @@
+'''
+Zippopotamus-Py
+-------
+
+Zippopotamus-Py is a library for Python that wraps the Zippopotamus API http://zippopotam.us/.
+
+Questions, Comments, Suggestions? akpratyush@gmail.com
+'''
+
+__author__ = 'Pratyush <akpratyush@gmail.com>'
+__version__ = '1.0'
+
 from zippopotamus import Zippopotamus
